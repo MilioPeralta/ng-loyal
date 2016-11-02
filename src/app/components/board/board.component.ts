2f2b58a41c6ed7301a0e8984768e6876b7236ea2
mperalta <mperalta@ipsosenso.com>
2016-11-02 15:04:24 +0100

    start round playing with Observable

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Contestant } from "../../models/Contestant";
 import { ContestantService } from "../../services/contestant.service";
+import { Observable } from "rxjs";
 
 
 @Component({
@@ -12,6 +13,9 @@ import { ContestantService } from "../../services/contestant.service";
 export class BoardComponent implements OnInit {
 
   private contestants: Contestant[];
+  private contestantsPerRound: { [key: number]: Contestant[] } = {};
+  private roundTime: number = 2000;
+  private roundNumber: number = 7;
 
   // The constructor itself does nothing.
   // The parameter simultaneously defines a private contestantService property
@@ -19,11 +23,31 @@ export class BoardComponent implements OnInit {
   constructor(private contestantService: ContestantService) {
   }
 
+  round(roundNumber: number): void {
+    let currentContestants: Contestant[] = this.contestantsPerRound[roundNumber];
+
+    console.log("round " + roundNumber + " playing : " + currentContestants[0].name + " vs " + currentContestants[1].name);
+    this.contestantsPerRound[roundNumber + 1] = currentContestants.slice(1, currentContestants.length);
+  }
+
+  run() {
+    console.log("run start...");
+
+    var obs = Observable
+      .interval(this.roundTime)
+      .take(this.roundNumber)
+      .do(roundNumber => this.round(roundNumber))
+      .finally(() => console.log("run end..."));
+
+    obs.subscribe();
+
+  }
 
   ngOnInit() {
-    this.contestants = this.contestantService.getContestants();
+    this.contestants = this.contestantsPerRound[0] = this.contestantService.getContestants();
 
-
+    // Let's go !
+    this.run();
   }
 
 }
