f8fc2231161d58b6a7ef703b7a42178080598972
Milio Peralta <milio.peralta@gmail.com>
2016-11-03 00:04:03 +0100

    add transitions

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,53 +1,77 @@
-import { Component, OnInit } from "@angular/core";
-import { Contestant } from "../../models/Contestant";
-import { ContestantService } from "../../services/contestant.service";
-import { Observable } from "rxjs";
+import {
+    Component,
+    OnInit
+} from "@angular/core";
+import {Contestant} from "../../models/Contestant";
+import {ContestantService} from "../../services/contestant.service";
+import {Observable} from "rxjs";
 
 
 @Component({
-  selector: 'app-board',
-  templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css'],
-  providers: [ContestantService]
+    selector: 'app-board',
+    templateUrl: './board.component.html',
+    styleUrls: ['./board.component.css'],
+    providers: [ContestantService]
 })
 export class BoardComponent implements OnInit {
 
-  private contestants: Contestant[];
-  private contestantsPerRound: { [key: number]: Contestant[] } = {};
-  private roundTime: number = 2000;
-  private roundNumber: number = 7;
+    private contestantsTmp: Contestant[];
+    private contestants: Contestant[] = [];
+    private winners: Contestant[] = [];
+    private contestantsPerRound: { [key: number]: Contestant[] } = {};
+    private roundTime: number = 2000;
+    private roundNumber: number = 7;
 
-  // The constructor itself does nothing.
-  // The parameter simultaneously defines a private contestantService property
-  // and identifies it as a ContestantService injection site.
-  constructor(private contestantService: ContestantService) {
-  }
+    // The constructor itself does nothing.
+    // The parameter simultaneously defines a private contestantService property
+    // and identifies it as a ContestantService injection site.
+    constructor(private contestantService: ContestantService) {
+    }
 
-  round(roundNumber: number): void {
-    let currentContestants: Contestant[] = this.contestantsPerRound[roundNumber];
+    round(roundNumber: number): void {
+        let currentContestants: Contestant[] = this.contestantsPerRound[roundNumber];
+        let offset = roundNumber === 3 || roundNumber === 5 ? 1 : 0;
 
-    console.log("round " + roundNumber + " playing : " + currentContestants[0].name + " vs " + currentContestants[1].name);
-    this.contestantsPerRound[roundNumber + 1] = currentContestants.slice(1, currentContestants.length);
-  }
+        let randomWinnerIndex = Math.floor(Math.random() * 2) + offset;
+        this.winners.push(currentContestants[randomWinnerIndex]);
 
-  run() {
-    console.log("run start...");
+        let loserIndex = (randomWinnerIndex + 1 + offset) % 2 + offset;
 
-    var obs = Observable
-      .interval(this.roundTime)
-      .take(this.roundNumber)
-      .do(roundNumber => this.round(roundNumber))
-      .finally(() => console.log("run end..."));
+        console.log(
+            "round " + roundNumber + " playing : " + currentContestants[0 + offset].id + " " + currentContestants[0 + offset].name +
+            " vs " +
+            currentContestants[1 + offset].id + " " + currentContestants[1 + offset].name
+        );
+        console.log("winner : ", currentContestants[randomWinnerIndex].id)
+        console.log("loser : ", currentContestants[loserIndex].id)
 
-    obs.subscribe();
+        this.contestantsPerRound[roundNumber + 1] = [...currentContestants.slice(0, loserIndex), ...currentContestants.slice(loserIndex + 1)];
+    }
 
-  }
+    run() {
+        console.log("run start...");
 
-  ngOnInit() {
-    this.contestants = this.contestantsPerRound[0] = this.contestantService.getContestants();
+        var obs = Observable
+            .interval(500)
+            .take(this.contestantsTmp.length)
+            .do(index => this.contestants.push(this.contestantsTmp[index]))
+            .finally(() => obs2.subscribe());
 
-    // Let's go !
-    this.run();
-  }
+        var obs2 = Observable
+            .interval(this.roundTime)
+            .take(this.roundNumber)
+            .do(roundNumber => this.round(roundNumber))
+            .finally(() => console.log("run end..."));
+
+        obs.subscribe();
+
+    }
+
+    ngOnInit() {
+        this.contestantsTmp = this.contestantsPerRound[0] = this.contestantService.getContestants();
+
+        // Let's go !
+        this.run();
+    }
 
 }
--- src/app/components/contestant/contestant.component.ts
+++ src/app/components/contestant/contestant.component.ts
@@ -1,20 +1,54 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Contestant } from "../../models/Contestant";
+import {
+    Component,
+    OnInit,
+    Input,
+    trigger,
+    state,
+    style,
+    transition,
+    animate
+} from '@angular/core';
+import {Contestant} from "../../models/Contestant";
 
 @Component({
-  selector: 'app-contestant',
-  templateUrl: './contestant.component.html',
-  styleUrls: ['./contestant.component.css']
+    selector: 'app-contestant',
+    templateUrl: './contestant.component.html',
+    styleUrls: ['./contestant.component.css'],
+    animations: [
+        trigger('fadeIn', [
+            state('true', style({opacity: 1, transform: 'scale(1.0)'})),
+            state('false', style({opacity: 0, transform: 'scale(0.0)'})),
+            transition('1 => 0', animate('300ms')),
+            transition('0 => 1', animate('900ms'))
+        ]),
+        trigger('flyInOut', [
+            state('in', style({transform: 'translateX(0)'})),
+            transition('void => *', [
+                style({transform: 'translateX(-100%)'}),
+                animate(100)
+            ]),
+            transition('* => void', [
+                animate(100, style({transform: 'translateX(100%)'}))
+            ])
+        ])
+    ]
 })
 export class ContestantComponent implements OnInit {
 
-  @Input()
-  private contestant: Contestant;
 
-  constructor() {
-  }
+    @Input()
+    private contestant: Contestant;
+    private state: Boolean = false;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    toggleState() {
+        this.state = !this.state;
+    }
+
+    ngOnInit() {
+        setTimeout(() => this.toggleState())
+    }
 
 }
