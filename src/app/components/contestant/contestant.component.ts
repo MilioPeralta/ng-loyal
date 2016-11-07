f65e63f47fcda5d38f6b351be0e65674b9188205
Milio Peralta <milio.peralta@gmail.com>
2016-11-08 00:04:40 +0100

    add flexbox grid

--- src/app/components/contestant/contestant.component.ts
+++ src/app/components/contestant/contestant.component.ts
@@ -22,13 +22,13 @@ import { Contestant } from "../../models/Contestant";
       transition('0 => 1', animate('900ms'))
     ]),
     trigger('flyInOut', [
-      state('in', style({ transform: 'translateX(0)' })),
+      state('in', style({ transform: 'translateY(0)' })),
       transition('void => *', [
-        style({ transform: 'translateX(-100%)' }),
+        style({ transform: 'translateY(-100%)' }),
         animate(100)
       ]),
       transition('* => void', [
-        animate(100, style({ transform: 'translateX(100%)' }))
+        animate(100, style({ transform: 'translateY(100%)' }))
       ])
     ])
   ]
